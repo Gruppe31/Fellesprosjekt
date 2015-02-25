c17394a0c09947d5311f0f11fa43f9330beca55d
oysteiae <oystein.aas.eide@gmail.com>
2015-02-25 10:52:28 +0100

    La til avtale og Person

---
+++ KalenderApplikasjon/src/model/Avtale.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Avtale {
+	private LocalTime tid;
+	private LocalDate dato;
+	private Rom rom;
+	private ArrayList<Person> folkSomKommer;
+	
+	
+}
---
+++ KalenderApplikasjon/src/model/Person.java
@@ -0,0 +1,66 @@
+package model;
+
+
+import java.security.KeyStore.PasswordProtection;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.PasswordField;
+
+public class Person {
+	
+	private StringProperty FornavnProperty = new SimpleStringProperty();
+	private StringProperty EtternavnProperty = new SimpleStringProperty();
+	private StringProperty BrukernavnProperty = new SimpleStringProperty();
+	private PasswordField PassordField = new PasswordField();
+		
+
+	public String getFornavn() {
+		return FornavnProperty.getValue();
+	}
+
+	public void setFornavn(String Fornavn) {
+		FornavnProperty.setValue(Fornavn);
+	}
+
+	public StringProperty FornavnProperty() {
+		return FornavnProperty;
+	}
+
+	public String getEtternavn() {
+		return EtternavnProperty.getValue();
+	}
+
+	public void setEtternavn(String rom) {
+		EtternavnProperty.setValue(rom);
+	}
+
+	public StringProperty EtternavnProperty() {
+		return EtternavnProperty;
+	}
+	
+	public StringProperty BrukernavnProperty(){
+		return BrukernavnProperty;
+	}
+	
+	public String getBrukernavn(){
+		return FornavnProperty.getValue();
+	}
+	
+	public void setBrukernavn(String Brukernavn){
+		BrukernavnProperty.setValue(Brukernavn);
+	}
+	
+	public PasswordField PassordField(){
+		return PassordField;
+	}
+	
+	public String getPassord(){
+		return PassordField.getText();
+	}
+	
+	public void setPassord(String Passord){
+		PassordField.setText(Passord);
+	}
+
+}
