b5f523f19d99556a739e851ec294c08c2f71dee0
oysteiae <oystein.aas.eide@gmail.com>
2015-02-25 11:16:49 +0100

    Endret avtale

--- KalenderApplikasjon/src/model/Avtale.java
+++ KalenderApplikasjon/src/model/Avtale.java
@@ -10,5 +10,40 @@ public class Avtale {
 	private Rom rom;
 	private ArrayList<Person> folkSomKommer;
 	
+	Avtale(LocalTime tid, LocalDate dato, Rom rom, ArrayList<Person> person){
+		this.tid = tid;
+		this.dato = dato;
+		this.rom = rom;
+		this.person = person;
+	}
+	
+	setTid(LocalTime tid){
+		this.tid = tid
+	}
+	
+	setDato(LocalDate dato){
+		this.dato = dato;
+	}
+	
+	setRom(Rom rom){
+		this.rom = rom;
+	}
+
+	public LocalTime getTid() {
+		return tid;
+	}
+
+	public LocalDate getDato() {
+		return dato;
+	}
+
+	public Rom getRom() {
+		return rom;
+	}
+
+	public ArrayList<Person> getFolkSomKommer() {
+		return folkSomKommer;
+	}
+	
 	
 }
