87df737b8042c4cb3d4e5bf39f754a9cdbfd3a87
flyingmaskin <maaugr@gmail.com>
2015-02-25 11:23:37 +0100

    Connector

--- KalenderApplikasjon/src/mysql/Connector.java
+++ KalenderApplikasjon/src/mysql/Connector.java
@@ -11,41 +11,24 @@ import java.util.Date;
 public class Connector {
   private Connection connect = null;
   private Statement statement = null;
-  private PreparedStatement preparedStatement = null;
   private ResultSet resultSet = null;
 
-  public void readDataBase() throws Exception {
+  public ResultSet les(String sqlstatement) throws Exception {
     try {
       Class.forName("com.mysql.jdbc.Driver");
-      connect = DriverManager.getConnection("jdbc:mysql://localhost/feedback?" + "user=oystein&password=sqluserpw");
+      connect = DriverManager.getConnection("jdbc:mysql://localhost/fp?" + "user=root&password=passwd");
 
       statement = connect.createStatement();
-      resultSet = statement.executeQuery("SELECT * FROM comments");
-      writeResultSet(resultSet);
-      
+      resultSet = statement.executeQuery(sqlstatement);
+      return resultSet;
     } catch (Exception e) {
-    	throw e;
+      throw e;
     } finally {
       close();
     }
 
   }
-
-
-  private void writeResultSet(ResultSet resultSet) throws SQLException {
-    while (resultSet.next()) {
-      String user = resultSet.getString("myuser");
-      String website = resultSet.getString("webpage");
-      String summary = resultSet.getString("summary");
-      Date date = resultSet.getDate("datum");
-      String comment = resultSet.getString("comments");
-      System.out.println("User: " + user);
-      System.out.println("Website: " + website);
-      System.out.println("summary: " + summary);
-      System.out.println("Date: " + date);
-      System.out.println("Comment: " + comment);
-    }
-  }
+  
   private void close() {
     try {
       if (resultSet != null) {
